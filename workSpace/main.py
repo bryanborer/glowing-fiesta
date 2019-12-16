ce689eb1cecb187dc89c3012a3a32e1e74c6a35e
Bryan Borer <bryanborer28@gmail.com>
2019-12-16 11:47:48 -0600

    Added network connection capability

--- workSpace/main.py
+++ workSpace/main.py
@@ -2,33 +2,41 @@ import time
 import network 
 from machine import Pin
 
-DELAY = 0.0055
+DELAY = 0.0055  # Delay between writing to each digit of HEX
 
-segA = Pin(16, Pin.OUT)
-segB = Pin(27, Pin.OUT)
-segC = Pin(26, Pin.OUT)
-segD = Pin(25, Pin.OUT)
-segE = Pin(33, Pin.OUT)
-segF = Pin(4, Pin.OUT)
-segG = Pin(14, Pin.OUT)
-dp = Pin(32, Pin.OUT)
+segA = Pin(16, Pin.OUT)  # A
+segB = Pin(27, Pin.OUT)  # B
+segC = Pin(26, Pin.OUT)  # C
+segD = Pin(25, Pin.OUT)  # D
+segE = Pin(33, Pin.OUT)  # E
+segF = Pin(4, Pin.OUT)   # F
+segG = Pin(14, Pin.OUT)  # G
+dp = Pin(32, Pin.OUT)    # Decimal Point
 
-dig1 = Pin(19, Pin.OUT)
-dig2 = Pin(18, Pin.OUT)
-dig3 = Pin(5, Pin.OUT)
-dig4 = Pin(17, Pin.OUT)
+dig1 = Pin(19, Pin.OUT)  # Digit 1
+dig2 = Pin(18, Pin.OUT)  # Digit 2
+dig3 = Pin(5, Pin.OUT)   # Digit 3
+dig4 = Pin(17, Pin.OUT)  # Digit 4
 
-sta_if = network.WLAN(network.STA_IF)
-sta_if.active(True)
-sta_if.connect('ALLOE6CC5', 'magicalbird445')
 
 def main():
-  print(time.time())
+  #do_connect()
   while True:
     four(1)
     two(2)
     zero(3)
     nine(4)
+    
+def do_connect():
+    import network
+    sta_if = network.WLAN(network.STA_IF)  # Instantiate station object
+    if not sta_if.isconnected():
+        print('connecting to network...')
+        sta_if.active(True)  # Activate station interface
+        sta_if.connect('ALLOE6CC5', 'magicalbird445')  # Connect to network
+        while not sta_if.isconnected():
+            pass
+    print('network config:', sta_if.ifconfig())
 
 def allLow():
   segA.value(0)
