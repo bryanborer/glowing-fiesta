7bc91e31e3a65cda476211f8f9a61c695e0b646e
Bryan Borer <bryanborer28@gmail.com>
2019-12-23 12:16:20 -0600

    Fixed time

--- workSpace/main.py
+++ workSpace/main.py
@@ -1,5 +1,6 @@
 import utime
 import ntptime
+import math
 from machine import Pin
 
 DELAY = 0.0055  # Delay between writing to each digit of HEX
@@ -20,25 +21,83 @@ dig4 = Pin(17, Pin.OUT)  # Digit 4
 
 
 def main():
-  do_connect()
-  ntptime.settime()
-  print(utime.localtime())
-  while True:
-    four(1)
-    two(2)
-    zero(3)
-    nine(4)
+  # Setup section
+  do_connect()                            # Connect to internet
+  ntptime.settime()                       # Set machine time to internet time
+  current_time = utime.time()              # Get time since program start
+  
+  # Calculate time for the first time so vars aren't null
+  hour, minute = calculate_time()
+  firstDigH = math.floor(hour/10)
+  firstDigM = math.floor(minute/10)
+  
+  #while True:
+  #  one(1)
+  #  two(2)
+  #  three(3)
+  #  four(4)
+  
+  while True:  # Run forever to continually display correct time
+    if utime.time() - current_time > 5:  # Check the current time every 5 seconds as to not hog CPU
+      hour, minute = calculate_time()
+      firstDigH = math.floor(hour/10)
+      firstDigM = math.floor(minute/10)
+      
+    print_number(firstDigH, 1)
+    print_number(hour - firstDigH * 10, 2)
+    
+    
+    print_number(firstDigM, 3)
+    print_number(minute - firstDigM * 10, 4)
     
 def do_connect():
-    import network
-    sta_if = network.WLAN(network.STA_IF)  # Instantiate station object
-    if not sta_if.isconnected():
-        print('connecting to network...')
-        sta_if.active(True)  # Activate station interface
-        sta_if.connect('ALLOE6CC5', 'magicalbird445')  # Connect to network
-        while not sta_if.isconnected():
-            pass
-    print('network config:', sta_if.ifconfig())
+  import network
+  sta_if = network.WLAN(network.STA_IF)  # Instantiate station object
+  if not sta_if.isconnected():
+    print('connecting to network...')
+    sta_if.active(True)  # Activate station interface
+    sta_if.connect('ALLOE6CC5', 'magicalbird445')  # Connect to network
+    while not sta_if.isconnected():
+      pass
+  print('network config:', sta_if.ifconfig())
+    
+def calculate_time():
+  year, month, mday, hour, minute, second, weekday, yearday = utime.localtime()  # Returns time in GMT, 6 hours ahead of CST
+  delta = hour - 6
+  
+  if delta <= 0:
+    mday = mday - 1  # Rewind a day 
+    hour = 24 + delta - 12  # Convert to CST (non-military time)
+  else:
+    hour = hour - 6  # Adjust time for CST
+    if hour > 12:
+      hour = hour - 12  # Convert to non-military time
+  
+  # Only need hour and minute for now
+  return hour, minute
+  
+  
+def print_number(number, digit):
+  if number == 0:
+    zero(digit)
+  if number == 1:
+    one(digit)
+  if number == 2:
+    two(digit)
+  if number == 3:
+    three(digit)
+  if number == 4:
+    four(digit)
+  if number == 5:
+    five(digit)
+  if number == 6:
+    six(digit)
+  if number == 7:
+    seven(digit)
+  if number == 8:
+    eight(digit)
+  if number == 9:
+    nine(digit)
 
 def allLow():
   segA.value(0)
@@ -237,5 +296,8 @@ def deselectAll():
 if __name__ == '__main__':
   main()
   
+
+
+
 
 
