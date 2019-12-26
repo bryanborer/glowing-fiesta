611c876a6761e6d6d60d1764f796ed76b2c2718c
Bryan Borer <bryanborer28@gmail.com>
2019-12-26 16:20:29 -0600

    Added more code comments and activated the second decimal point on the HEX display

--- src/c/hex/hex.ino
+++ src/c/hex/hex.ino
@@ -1,27 +1,30 @@
 #include <time.h>
 #include <WiFi.h>
 #include <WiFiClient.h>
-const unsigned long DELAY = 5;
 
-const int segA = 16;
-const int segB = 27;
-const int segC = 26;
-const int segD = 25;
-const int segE = 33;
-const int segF = 4;
-const int segG = 14;
-const int dp = 32;
+const unsigned long DELAY = 5; // Must delay in between writing to HEX Display to not cause overlap
 
-const int dig1 = 19;
-const int dig2 = 18;
-const int dig3 = 5;
-const int dig4 = 17;
+// These pin assignments are according to this diagram:
+//    https://github.com/thehookup/ESP32_Ceiling_Light/blob/master/GPIO_Limitations_ESP32_NodeMCU.jpg
+const int segA = 16; // A
+const int segB = 27; // B
+const int segC = 26; // C
+const int segD = 25; // D
+const int segE = 33; // E
+const int segF = 4;  // F
+const int segG = 14; // G
+const int dp = 32;   // Decimal Point
+
+const int dig1 = 19; // Digit 1
+const int dig2 = 18; // Digit 2
+const int dig3 = 5;  // Digit 3
+const int dig4 = 17; // Digit 4
 
 const char *ssid = "ALLOE6CC5";
 const char *password = "magicalbird445";
 
-unsigned long currentMillis = 0;
-time_t now = time(nullptr);
+unsigned long currentMillis = 0; // Keeps track of current milliseconds passed since program start 
+time_t now = time(nullptr);      // Current real time
 
 void setup(){
   // Start serial port for ESP32
@@ -58,7 +61,8 @@ void setup(){
   Serial.println(WiFi.localIP());
 
   // Get time from internet
-  // void configTime(int timezone, int daylightOffset_sec, const char* server1, const char* server2, const char* server3)
+  //    void configTime(int timezone, int daylightOffset_sec, const char* server1, const char* server2, const char* server3)
+  //    (-6) represents Central Standard Time
   configTime((-6) * 3600, 0, "pool.ntp.org", "time.nist.gov");
   Serial.println("\nWaiting for time");
   while (!time(nullptr)) {
@@ -67,8 +71,8 @@ void setup(){
   }
   Serial.println("");
 
-  delay(1000); // Wait for time to refresh
-  now = time(nullptr);
+  delay(1000);          // Wait for time to be retrieved from internet
+  now = time(nullptr);  // Update real time variable
   Serial.println(ctime(&now));
   // Serial.println(ctime(&now)[11]);
   // Serial.println((char)ctime(&now)[11]);
@@ -84,7 +88,7 @@ void loop(){
     currentMillis = millis();
   }
 
-  // Must subtract 48 due to ascii conversion: '0' is number 48
+  // Must subtract 48 due to ascii conversion from char to int i.e. (int)'0' = 48
   print_number(convert_hour((int)ctime(&now)[11] - 48, (int)ctime(&now)[12] - 48, 0), 1);
   print_number(convert_hour((int)ctime(&now)[11] - 48, (int)ctime(&now)[12] - 48, 1), 2);
   print_number((int)ctime(&now)[14] - 48, 3);
@@ -92,6 +96,10 @@ void loop(){
 }
 
 int convert_hour(int first, int second, bool digit){
+  /*
+   * Converts the first and second digits of the hour into normal time non-military time
+   */
+
   if(first == 1 && second > 2){ // Hour: 12 <= x <= 19
     first -= 1;
     second -= 2;
@@ -110,6 +118,10 @@ int convert_hour(int first, int second, bool digit){
 }
 
 void print_number(int number, int digit){
+  /*
+   * Prints the desired number 0-9 on the desired digit 1-4
+   */
+
   if(number == 0){
     zero(digit);
   }
@@ -143,6 +155,9 @@ void print_number(int number, int digit){
 }
 
 void all_low(){
+  /*
+   * Turns off all segments of the current digit
+   */
   digitalWrite(segA, 0);
   digitalWrite(segB, 0);
   digitalWrite(segC, 0);
@@ -154,6 +169,9 @@ void all_low(){
 }
 
 void all_high(){
+  /*
+   * Turns on all segments of the current digit
+   */
   digitalWrite(segA, 1);
   digitalWrite(segB, 1);
   digitalWrite(segC, 1);
@@ -174,7 +192,7 @@ void zero(int digit){
   digitalWrite(segE, 1);
   digitalWrite(segF, 1);
   digitalWrite(segG, 0);
-  digitalWrite(dp, 0);
+  check_decimal(digit);
   delay(DELAY);
 }
 
@@ -188,7 +206,7 @@ void one(int digit){
   digitalWrite(segE, 0);
   digitalWrite(segF, 0);
   digitalWrite(segG, 0);
-  digitalWrite(dp, 0);
+  check_decimal(digit);
   delay(DELAY);
 }
 
@@ -202,7 +220,7 @@ void two(int digit){
   digitalWrite(segE, 1);
   digitalWrite(segF, 0);
   digitalWrite(segG, 1);
-  digitalWrite(dp, 0);
+  check_decimal(digit);
   delay(DELAY);
 }
 
@@ -216,7 +234,7 @@ void three(int digit){
   digitalWrite(segE, 0);
   digitalWrite(segF, 0);
   digitalWrite(segG, 1);
-  digitalWrite(dp, 0);
+  check_decimal(digit);
   delay(DELAY);
 }
 
@@ -230,7 +248,7 @@ void four(int digit){
   digitalWrite(segE, 0);
   digitalWrite(segF, 1);
   digitalWrite(segG, 1);
-  digitalWrite(dp, 0);
+  check_decimal(digit);
   delay(DELAY);
 }
 
@@ -244,7 +262,7 @@ void five(int digit){
   digitalWrite(segE, 0);
   digitalWrite(segF, 1);
   digitalWrite(segG, 1);
-  digitalWrite(dp, 0);
+  check_decimal(digit);
   delay(DELAY);
 }
 
@@ -258,7 +276,7 @@ void six(int digit){
   digitalWrite(segE, 1);
   digitalWrite(segF, 1);
   digitalWrite(segG, 1);
-  digitalWrite(dp, 0);
+  check_decimal(digit);
   delay(DELAY);
 }
 
@@ -272,7 +290,7 @@ void seven(int digit){
   digitalWrite(segE, 0);
   digitalWrite(segF, 0);
   digitalWrite(segG, 0);
-  digitalWrite(dp, 0);
+  check_decimal(digit);
   delay(DELAY);
 }
 
@@ -286,7 +304,7 @@ void eight(int digit){
   digitalWrite(segE, 1);
   digitalWrite(segF, 1);
   digitalWrite(segG, 1);
-  digitalWrite(dp, 0);
+  check_decimal(digit);
   delay(DELAY);
 }
 
@@ -300,7 +318,7 @@ void nine(int digit){
   digitalWrite(segE, 0);
   digitalWrite(segF, 1);
   digitalWrite(segG, 1);
-  digitalWrite(dp, 0);
+  check_decimal(digit);
   delay(DELAY);
 }
 
@@ -318,5 +336,16 @@ void select_digit(int digit){
     digitalWrite(dig3, 0);
   }else if(digit == 4){
     digitalWrite(dig4, 0);
+  }
+}
+
+void check_decimal(int digit){
+  /*
+   * Only need to turn on the decimal point between the 2nd and 3rd digits
+   */
+  if(digit == 2){
+    digitalWrite(dp, 1);
+  }else{
+    digitalWrite(dp, 0);
   }
 }
